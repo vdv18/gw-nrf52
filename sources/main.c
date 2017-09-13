892a8d76807834663080ad688799e132e103125e
Dmitry Vakhrushev <vdv.18@mail.ru>
2017-09-13 10:16:30 +0700

    Work SPI slave

--- sources/main.c
+++ sources/main.c
@@ -2,8 +2,8 @@
 #include "ble.h"
 #include "nrf_sdm.h"
 
-static uint8_t buffer_in[0x4];
-static uint8_t buffer_out[0x4];
+static uint8_t buffer_in[0x4] = {0x12,0x34,0x56,0x78};
+static uint8_t buffer_out[0x4] = {0x12,0x34,0x56,0x78};
 void delay(uint32_t tick)
 {
   volatile uint32_t temp = tick;
@@ -23,12 +23,15 @@ void SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0_IRQHandler(void)
     NRF_SPIS0->TXD.MAXCNT = sizeof(buffer_out);
     
     NRF_SPIS0->TASKS_RELEASE = 0x1UL;
+    NRF_SPIS0->EVENTS_ACQUIRED = 0x0;
   }
   if(NRF_SPIS0->EVENTS_ENDRX)
   {
+    NRF_SPIS0->EVENTS_ENDRX = 0x0;
   }
   if(NRF_SPIS0->EVENTS_END)
   {
+    NRF_SPIS0->EVENTS_END = 0x0;
   }
 //  NRF_SPIS0->INTENSET = (SPIS_INTENSET_ACQUIRED_Set << SPIS_INTENSET_ACQUIRED_Pos) | 
 //    (SPIS_INTENSET_ENDRX_Set << SPIS_INTENSET_ENDRX_Pos) |
@@ -57,8 +60,8 @@ void spis_init(void)
   NRF_SPIS0->TXD.MAXCNT = sizeof(buffer_out);
   
   
-  NRF_SPIS0->CONFIG = (SPIS_CONFIG_CPOL_ActiveHigh << SPIS_CONFIG_CPOL_Pos) | 
-    (SPIS_CONFIG_CPHA_Leading << SPIS_CONFIG_CPHA_Pos) |
+  NRF_SPIS0->CONFIG = (SPIS_CONFIG_CPOL_ActiveLow << SPIS_CONFIG_CPOL_Pos) | 
+    (SPIS_CONFIG_CPHA_Trailing << SPIS_CONFIG_CPHA_Pos) |
     (SPIS_CONFIG_ORDER_LsbFirst << SPIS_CONFIG_ORDER_Pos);
   
   NRF_SPIS0->DEF = 0xFF;  
@@ -72,10 +75,20 @@ void spis_init(void)
   NRF_SPIS0->EVENTS_END = 0;
   NRF_SPIS0->EVENTS_ACQUIRED = 0;
   NRF_SPIS0->ENABLE = 2;
+  
+  
+  
+  NRF_SPIS0->RXD.PTR = (uint32_t)buffer_in;
+  NRF_SPIS0->RXD.MAXCNT = sizeof(buffer_in);
+  
+  NRF_SPIS0->TXD.PTR = (uint32_t)buffer_out;
+  NRF_SPIS0->TXD.MAXCNT = sizeof(buffer_out);
+  NRF_SPIS0->TASKS_ACQUIRE = 1;
 }
 void main()
 {
   static uint32_t pins = 0;
+  SCB->VTOR = 0x23000;
   NRF_P0->DIRCLR = 0x1F<<11;
   
   NRF_P0->PIN_CNF[11] = 0;
